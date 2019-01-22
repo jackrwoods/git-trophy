1191f3c2d8c7b90f7f0bd67ac504cb64d5ce680b
Brogan Miner <BroganMiner22@gmail.com>
2019-01-22 09:45:09 -0800

    add jest

---
+++ tests/example.test.js
@@ -0,0 +1,10 @@
+/**
+ * @Author: Brogan Miner <Brogan>
+ * @Date:   2019-01-22T09:40:29-08:00
+ * @Email:  brogan.miner@oregonstate.edu
+ * @Last modified by:   Brogan
+ * @Last modified time: 2019-01-22T09:43:41-08:00
+ */
+test('This is an example test', () => {
+  expect(true === true)
+})
