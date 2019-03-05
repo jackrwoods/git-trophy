24632fa178c893ff7a2270aea8363d53a56745b7
Brogan Miner <BroganMiner22@gmail.com>
2019-03-05 10:06:53 -0800

    rach

--- tests/RepoSelector.test.js
+++ tests/RepoSelector.test.js
@@ -3,7 +3,7 @@
  * @Date:   2019-02-28
  * @Email:  sousar@oregonstate.edu
  * @Last modified by:   Brogan
- * @Last modified time: 2019-03-05T09:51:52-08:00
+ * @Last modified time: 2019-03-05T10:05:12-08:00
  */
 
 import React from 'react' // Import react
@@ -34,13 +34,13 @@ test('handleDropdownChange Test', () => {
   // eslint-disable-next-line
   const fakeData = new Array(3)
   for (let index in fakeData) {
-    fakeData[index] = {}
+    fakeData[index] = index.toString()
   }
   let num = 0
   const component = shallow(
-    <RepoSelector entity='rachel-sousa' updateSelectedEntity={() => {}} updateSelectedYear={function () { this.yearOptions[0] = 1 }} year='2018' yearOptions={fakeData} />
+    <RepoSelector entity='rachel-sousa' updateSelectedEntity={() => {}} updateSelectedYear={v => { component.props.year = component.props.yearOptions[v] }} year='2018' yearOptions={fakeData} />
   ).instance()
-  component.handleDropdownChange({}, {})
-  expect(component.yearOptions[0] === 1)
+  component.handleDropdownChange({}, { value: 0 })
+  expect(component.props.year[0] === 0)
   // expect(handleEntityChange())
 })
